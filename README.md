fb8688ff43539a1c5442fcfe8144da02639ba84d
headironc <i@liangzhicheng.cc>
2023-10-08 20:43:22 +0800

    chore: initialize

---
+++ README.md
@@ -0,0 +1,5 @@
+# open-wechat
+
+![WeChat](https://img.shields.io/badge/WeChat-07C160?style=for-the-badge&logo=wechat&logoColor=white)
+
+A rust sdk for wechat miniprogram open api
---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+/// test
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+}
