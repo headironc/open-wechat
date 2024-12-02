88fc05c9458598e9dc82a3c90d2737c884a69ef3
headiron <i@liangzhicheng.cc>
2024-12-02 19:49:00 +0800

    Add access token retrieval functionality and update routes (#34)
    * fix: add access token retrieval functionality and update routes
    
    * fix: update access token route to use hyphenated naming convention
    
    * bump: update version to 1.0.3 in Cargo.toml

--- src/interfaces/mod.rs
+++ src/interfaces/mod.rs
@@ -1,18 +1,20 @@
 mod users;
 
 use axum::Router;
-use open_wechat::client::Client;
+use open_wechat::{client::Client, credential::GenericAccessToken};
 use tower_http::trace::{
     DefaultMakeSpan, DefaultOnFailure, DefaultOnRequest, DefaultOnResponse, TraceLayer,
 };
 use tracing::Level;
 
-pub(crate) fn routes(client: Client) -> Router {
-    Router::new().nest("/users", users::routes(client)).layer(
-        TraceLayer::new_for_http()
-            .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
-            .on_request(DefaultOnRequest::new().level(Level::INFO))
-            .on_response(DefaultOnResponse::new().level(Level::INFO))
-            .on_failure(DefaultOnFailure::new().level(Level::ERROR)),
-    )
+pub(crate) fn routes(client: Client, access_token: GenericAccessToken) -> Router {
+    Router::new()
+        .nest("/users", users::routes(client, access_token))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_response(DefaultOnResponse::new().level(Level::INFO))
+                .on_failure(DefaultOnFailure::new().level(Level::ERROR)),
+        )
 }
--- src/interfaces/users.rs
+++ src/interfaces/users.rs
@@ -1,11 +1,18 @@
-use axum::{routing::post, Router};
-use open_wechat::client::Client;
+use axum::{
+    routing::{get, post},
+    Router,
+};
+use open_wechat::{client::Client, credential::GenericAccessToken};
 
-use crate::controllers::users::{decrypt, login};
+use crate::controllers::users::{decrypt, get_access_token, login};
 
-pub(super) fn routes(client: Client) -> Router {
+pub(super) fn routes(client: Client, access_token: GenericAccessToken) -> Router {
     Router::new()
         .route("/login", post(login))
         .route("/decrypt", post(decrypt))
         .with_state(client)
+        .route(
+            "/access-token",
+            get(get_access_token).with_state(access_token),
+        )
 }
