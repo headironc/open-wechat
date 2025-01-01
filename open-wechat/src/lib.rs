8d5c3c5cc45767ebd3f3e8b37cf5db8c8f93a096
Archer Leung <i@liangzhicheng.cc>
2025-01-01 23:03:33 +0800

    feat: Add session key validation and reset functionality with async support

--- open-wechat/src/lib.rs
+++ open-wechat/src/lib.rs
@@ -100,6 +100,71 @@
 //!     Ok(())
 //! }
 //! ```
+//!
+//! ### Check session key
+//!
+//! ```rust
+//! use axum::{extract::State, response::IntoResponse, Json};
+//! use open_wechat::client::{GenericAccessToken, Client, CheckSessionKey};
+//! use serde::Deserialize;
+//!
+//! #[derive(Debug, Deserialize, Default)]
+//! #[serde(default)]
+//! pub(crate) struct SessionKeyChecker {
+//!     session_key: String,
+//!     open_id: String,
+//! }
+//!
+//! pub(crate) async fn check_session_key(
+//!     State(access_token): State<GenericAccessToken>,
+//!     JsonDecoder(checker): JsonDecoder<SessionKeyChecker>,
+//! ) -> Result<impl IntoResponse> {
+//!     event!(Level::INFO, "check session key: {:?}", checker);
+//!
+//!     access_token
+//!         .check_session_key(&checker.session_key, &checker.open_id)
+//!         .await?;
+//!
+//!     Ok(Json(json!({
+//!         "session_key": checker.session_key,
+//!         "open_id": checker.open_id,
+//!         "message": "session key is valid",
+//!     })))
+//! }
+//! ```
+//!
+//! ### Reset session key
+//!
+//! ```rust
+//! use axum::{extract::State, response::IntoResponse, Json};
+//! use open_wechat::client::{GenericAccessToken, Client, ResetSessionKey};
+//! use serde::Deserialize;
+//!
+//! use crate::Result;
+//!
+//! #[derive(Debug, Deserialize, Default)]
+//! #[serde(default)]
+//! pub(crate) struct SessionKeyResetter {
+//!     session_key: String,
+//!     open_id: String,
+//! }
+//!
+//! pub(crate) async fn reset_session_key(
+//!     State(access_token): State<GenericAccessToken>,
+//!     JsonDecoder(resetter): JsonDecoder<SessionKeyResetter>,
+//! ) -> Result<impl IntoResponse> {
+//!     event!(Level::INFO, "reset session key: {:?}", resetter);
+//!
+//!     let credential = access_token
+//!         .reset_session_key(&resetter.session_key, &resetter.open_id)
+//!         .await?;
+//!
+//!     Ok(Json(json!({
+//!         "message": "session key is reset",
+//!         "credential": credential,
+//!     })))
+//! }
+//! ```
 
 #![warn(
     clippy::all,
