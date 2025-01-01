2335f7377fdde03b25c2e37f6a6f1af7a46d1fd7
Archer Leung <i@liangzhicheng.cc>
2025-01-01 22:57:52 +0800

    feat: Add reset session key functionality and logging in user controller

--- src/interfaces/users.rs
+++ src/interfaces/users.rs
@@ -4,7 +4,9 @@ use axum::{
 };
 use open_wechat::{client::Client, credential::GenericAccessToken};
 
-use crate::controllers::users::{check_session_key, decrypt, get_access_token, login};
+use crate::controllers::users::{
+    check_session_key, decrypt, get_access_token, login, reset_session_key,
+};
 
 pub(super) fn routes(client: Client, access_token: GenericAccessToken) -> Router {
     Router::new()
@@ -13,5 +15,6 @@ pub(super) fn routes(client: Client, access_token: GenericAccessToken) -> Router
         .with_state(client)
         .route("/access-token", get(get_access_token))
         .route("/check-session-key", post(check_session_key))
+        .route("/reset-session-key", post(reset_session_key))
         .with_state(access_token)
 }
