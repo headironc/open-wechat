370cfdef78af226b9ffc19faa670f6db8b0f06cc
Archer Leung <i@liangzhicheng.cc>
2025-01-02 17:54:36 +0800

    refactor: restructure project by creating a new helper package and removing the old open-wechat package

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,184 @@
-mod controllers;
-mod error;
-mod interfaces;
-mod setup;
-mod trace;
-mod utils;
+//! # open-wechat
+//!
+//! ![WeChat](https://img.shields.io/badge/WeChat-07C160?style=for-the-badge&logo=wechat&logoColor=white)
+//!
+//! A rust sdk for wechat miniprogram open api
+//!
+//! ## Usage
+//!
+//! ### Get access token
+//!
+//! ```rust
+//! use open_wechat::{
+//!     client::Client,
+//!     credential::{GenericAccessToken, GetAccessToken}
+//! };
+//!
+//! #[tokio::main]
+//! async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     let app_id = "your app id";
+//!     let app_secret = "your app secret";
+//!
+//!     let client = Client::new(app_id, app_secret);
+//!
+//!     let access_token = GenericAccessToken::new(self.client.clone()).await?;
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! ### Get stable access token
+//!
+//! ```rust
+//! use open_wechat::{
+//!     client::Client,
+//!     credential::{GenericAccessToken, GetStableAccessToken}
+//! };
+//!
+//! #[tokio::main]
+//! async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     let app_id = "your app id";
+//!     let app_secret = "your app secret";
+//!
+//!     let client = Client::new(app_id, app_secret);
+//!
+//!     let stable_access_token = GenericAccessToken::new(self.client.clone(), None).await?;
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! ### Code to session
+//!
+//! ```rust
+//! use axum::{extract::State, response::IntoResponse, Json};
+//! use open_wechat::client::Client;
+//! use serde::Deserialize;
+//!
+//! use crate::Result;
+//!
+//! #[derive(Deserialize, Default)]
+//! #[serde(default)]
+//! pub(crate) struct Logger {
+//!     code: String,
+//! }
+//!
+//! pub(crate) async fn login(
+//!     State(client): State<Client>,
+//!     Json(logger): Json<Logger>,
+//! ) -> Result<impl IntoResponse> {
+//!     let credential = client.login(&logger.code).await?;
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! ### Decrypt data
+//!
+//! ```rust
+//! use axum::{extract::State, response::IntoResponse, Json};
+//! use open_wechat::client::Client;
+//! use serde::Deserialize;
+//!
+//! use crate::Result;
+//!
+//! #[derive(Deserialize, Default)]
+//! pub(crate) struct EncryptedPayload {
+//!     code: String,
+//!     encrypted_data: String,
+//!     iv: String,
+//! }
+//!
+//! pub(crate) async fn decrypt(
+//!     State(client): State<Client>,
+//!     Json(payload): Json<EncryptedPayload>,
+//! ) -> Result<impl IntoResponse> {
+//!     let credential = client.login(&payload.code).await?;
+//!
+//!     let user = credential.decrypt(&payload.encrypted_data, &payload.iv)?;
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! ### Check session key
+//!
+//! ```rust
+//! use axum::{extract::State, response::IntoResponse, Json};
+//! use open_wechat::client::{GenericAccessToken, Client, CheckSessionKey};
+//! use serde::Deserialize;
+//!
+//! #[derive(Debug, Deserialize, Default)]
+//! #[serde(default)]
+//! pub(crate) struct SessionKeyChecker {
+//!     session_key: String,
+//!     open_id: String,
+//! }
+//!
+//! pub(crate) async fn check_session_key(
+//!     State(access_token): State<GenericAccessToken>,
+//!     JsonDecoder(checker): JsonDecoder<SessionKeyChecker>,
+//! ) -> Result<impl IntoResponse> {
+//!     event!(Level::INFO, "check session key: {:?}", checker);
+//!
+//!     access_token
+//!         .check_session_key(&checker.session_key, &checker.open_id)
+//!         .await?;
+//!
+//!     Ok(Json(json!({
+//!         "session_key": checker.session_key,
+//!         "open_id": checker.open_id,
+//!         "message": "session key is valid",
+//!     })))
+//! }
+//! ```
+//!
+//! ### Reset session key
+//!
+//! ```rust
+//! use axum::{extract::State, response::IntoResponse, Json};
+//! use open_wechat::client::{GenericAccessToken, Client, ResetSessionKey};
+//! use serde::Deserialize;
+//!
+//! use crate::Result;
+//!
+//! #[derive(Debug, Deserialize, Default)]
+//! #[serde(default)]
+//! pub(crate) struct SessionKeyResetter {
+//!     session_key: String,
+//!     open_id: String,
+//! }
+//!
+//! pub(crate) async fn reset_session_key(
+//!     State(access_token): State<GenericAccessToken>,
+//!     JsonDecoder(resetter): JsonDecoder<SessionKeyResetter>,
+//! ) -> Result<impl IntoResponse> {
+//!     event!(Level::INFO, "reset session key: {:?}", resetter);
+//!
+//!     let credential = access_token
+//!         .reset_session_key(&resetter.session_key, &resetter.open_id)
+//!         .await?;
+//!
+//!     Ok(Json(json!({
+//!         "message": "session key is reset",
+//!         "credential": credential,
+//!     })))
+//! }
+//! ```
 
-use open_wechat::{
-    client::Client,
-    credential::{GenericAccessToken, GetAccessToken},
-};
-use tracing::{event, instrument, Level};
+#![warn(
+    clippy::all,
+    nonstandard_style,
+    future_incompatible,
+    missing_debug_implementations
+)]
+#![allow(rustdoc::bare_urls)]
+#![forbid(unsafe_code)]
+
+pub mod client;
+pub mod credential;
+pub mod error;
+mod response;
+pub mod user;
 
 pub type Result<T> = std::result::Result<T, error::Error>;
-
-pub struct Application {
-    _trace: trace::Trace,
-    setup: setup::Setup,
-    client: Client,
-}
-
-impl Application {
-    pub async fn new() -> Result<Self> {
-        let _trace = trace::Trace::new().await?;
-        let setup = setup::Setup::new()?;
-        let client = setup.client();
-
-        Ok(Self {
-            _trace,
-            setup,
-            client,
-        })
-    }
-
-    #[instrument(skip(self), name = "running")]
-    pub async fn run(&self) -> Result<()> {
-        let listener = self.setup.listener().await?;
-
-        let access_token = GenericAccessToken::new(self.client.clone()).await?;
-        // let _stable_access_token = GenericAccessToken::new(self.client.clone(), None).await?;
-
-        let routes = interfaces::routes(self.client.clone(), access_token.clone());
-
-        event!(Level::INFO, "on http://{}", listener.local_addr()?);
-
-        axum::serve(listener, routes).await?;
-
-        Ok(())
-    }
-}
